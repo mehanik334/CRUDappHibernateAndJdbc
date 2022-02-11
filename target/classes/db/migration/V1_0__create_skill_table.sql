99c538b79dc7fe2f9a9c197181503aa9741493ee
meha3 <meha334@gmail.com>
2022-02-11 05:43:04 +0200

    ignore target

--- target/classes/db/migration/V1_0__create_skill_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS skill
-(
-    skill_id int NOT NULL AUTO_INCREMENT primary key,
-    name     varchar(255) NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
--- target/classes/db/migration/V1_2__create_developer_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS developer
-(
-    id         int NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    first_name varchar(255),
-    last_name  varchar(255),
-    team_id    int,
-    foreign key (team_id) references team (id)
-        on delete cascade
-);
